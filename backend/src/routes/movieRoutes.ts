c528c514d2443a07f416d73fdc14f7d1753806a5
imayden <ayden.yiming.deng@gmail.com>
2024-07-15 11:10:46 -0400

    update

--- backend/src/routes/movieRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getMovies, fetchMovies } from '../controllers/movieController';
-
-const router = Router();
-
-router.get('/movies', getMovies);
-router.get('/fetch-movies', fetchMovies);
-
-export default router;
